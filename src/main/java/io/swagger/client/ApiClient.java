dfc789efd29cf2fd7311bfa0a0a8036ded85158d
Aidana <a.saduova@mymovie.guru>
2019-05-14 18:11:03 +0200

    changed path for local android usage

--- src/main/java/io/swagger/client/ApiClient.java
+++ src/main/java/io/swagger/client/ApiClient.java
@@ -52,7 +52,7 @@ import io.swagger.client.auth.OAuth;
 
 public class ApiClient {
 
-    private String basePath = "http://localhost:9000/api";
+    private String basePath = "http://10.0.2.2:9000/api";
     private boolean debugging = false;
     private Map<String, String> defaultHeaderMap = new HashMap<String, String>();
     private String tempFolderPath = null;
